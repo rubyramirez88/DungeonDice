cb5ccf972322cf24d50228a2f0abda34c68d8b7e
rubyramirez88 <ruizruby160@gmail.com>
2026-02-11 13:11:35 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by RAMIREZ, RUBY on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
